c977f712a163d54cb73908d4f04bb407715310db
Eli-b-good <eli.i.henderson95@gmail.com>
2020-09-22 15:16:36 -0400

    added cors module

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 require("./db/mongoose");
 const mangaRouter = require("./routers/manga");
 const userRouter = require("./routers/users");
+const cors = require("cors");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(mangaRouter);
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const cors = require("cors");
 const app = require("./app");
 
 const port = process.env.PORT;
-
+app.use(cors());
 app.get("", (req, res) => {
   res.send(`Welcome to the ricefields server`);
 });
-app.use(cors());
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
