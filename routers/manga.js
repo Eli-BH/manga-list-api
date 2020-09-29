aaa316a915eed23a2a047e61f9c606ad2c737218
Eli-b-good <eli.i.henderson95@gmail.com>
2020-09-29 17:50:15 -0400

    fixed the patch route for manga

--- routers/manga.js
+++ routers/manga.js
@@ -73,7 +73,7 @@ router.delete("/api/manga/:id", auth, async (req, res) => {
 //updata manga
 router.patch("/api/manga/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
-  const allowedUpdates = ["readChapterAmount", "complete"];
+  const allowedUpdates = ["readChapterAmount", "complete", "rating"];
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update)
   );
